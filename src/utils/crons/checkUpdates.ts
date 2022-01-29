58138719469435a75b04f88dbfe14508610a9fa6
Jõnathas Gouveia <jonathas_gouv@hotmail.com>
2022-01-29 00:45:26 -0300

    makes cron run everyday 9am

--- src/utils/crons/checkUpdates.ts
+++ src/utils/crons/checkUpdates.ts
@@ -1,6 +1,7 @@
 import cron from 'node-cron'
 import Article from '@models/Article'
 import IArticle from '@types/article'
+
 const axios = require('axios')
 
 // https://api.spaceflightnewsapi.net/v3/articles?_limit=999999999
@@ -10,21 +11,27 @@ const spaceflightnewsInstance = axios.create({
   timeout: 54000
 })
 
-/*
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 9 * * *', async () => {
   const response = await spaceflightnewsInstance.get('/articles', { params: { _limit: 100 } })
   const articles: IArticle[] = response.data
 
+  let newArticles = 0
+
   articles.forEach(async (article) => {
-    console.log(article)
-    const alreadyExists = await Article.findById(article.id).orFail()
+    article._id = article.id
+    delete article.id
+
+    const alreadyExists = await Article.findById(article._id).orFail()
 
     if (!alreadyExists) {
       await Article.create(article, (error) => {
-        console.info('There was an error creating the article with id ' + article.id)
+        console.info('There was an error creating the article with id ' + article._id)
         console.table(error)
       })
+
+      newArticles++
     }
   })
+
+  console.info(`Cron ran successfully. ${newArticles} articles created.`)
 })
-*/
